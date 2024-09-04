285105fa83e18255ac47548cb2a6eea320f0db2c
Devabhaktineni Sayendranadh Chowdary <devabhaktinenisayendra@gmail.com>
2024-09-03 20:26:27 -0500

    Create mock.js

---
+++ src/utils/mock.js
@@ -0,0 +1,169 @@
+
+const rest_data=[
+    {
+      city: "Maryville",
+      state:"MO",
+      review:4.5,
+      rest_name:"Pizza-Hut",
+      Items:["Big Dipper Box", "Cinnamon sticks", "Stuffed Crust", "Supreme Pan with butter garlic crust","Pan Pizza","Bread Bites", "Buffalo Chicken"],
+      prep_time: "25min",
+      img_link:"https://tb-static.uber.com/prod/image-proc/processed_images/0a59c36193251022e816ffec42d9870d/30be7d11a3ed6f6183354d1933fbb6c7.jpeg"
+  
+  
+    },
+  
+    {
+      city: "Maryville",
+      state:"MO",
+      review:4.5,
+      rest_name:"Dominos",
+      Items:["Pepperoni Pizza", "Cheese Burst Pizza", "Choco Lava Cake", "Garlic Bread", "Chicken Wings"],
+      prep_time: "20min",
+      img_link:"https://www.thestreet.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc1OTU4MzE2Mzk1NDcyNzc1/2_dominos-store_eugene-or-e1546984571327-800x400.jpg"
+  
+  
+    },
+  
+    {
+      city: "Maryville",
+      state:"MO",
+      review:4.5,
+      rest_name:"Taco-Bell",
+      Items:[
+        "Crunchy Taco",
+        "Burrito Supreme",
+        "Chicken Quesadilla",
+        "Nacho Fries",
+        "Cheesy Gordita Crunch",
+        "Cinnamon Twists"
+      ],
+      prep_time: "15min",
+      img_link:"https://thumbs.dreamstime.com/b/pink-purple-logo-taco-bell-fast-food-establishment-northern-california-taken-nikon-d-185730391.jpg"
+  
+  
+    },
+  
+    
+    {
+      city: "Maryville",
+      state:"MO",
+      review:4.5,
+      rest_name:"Mc_Donald",
+      Items:[
+        "Big Mac",
+        "Quarter Pounder with Cheese",
+        "McNuggets",
+        "Filet-O-Fish",
+        "McChicken",
+        "Egg McMuffin",
+        "Happy Meal",
+        "French Fries",
+        "McFlurry",
+        "Sundae"
+      ],
+      prep_time: "5min",
+      img_link:"https://cloudfront-us-east-2.images.arcpublishing.com/reuters/DVTSRAYLERNWNNZNKS6OCO3Q7M.jpg"
+  
+  
+    },
+  
+    {
+      city: "Maryville",
+      state:"MO",
+      review:4.5,
+      rest_name:"Casey's",
+      Items:[
+        "Casey's Classic Pizza",
+        "Breakfast Pizza",
+        "Taco Pizza",
+        "Cheeseburger Pizza",
+        "Pepperoni Pizza",
+        "Chicken Bacon Ranch Pizza",
+        "Casey's Sub Sandwiches",
+        "Chicken Tenders",
+        "Breadsticks",
+        "Casey's Wings"
+      ],
+      prep_time: "5min",
+      img_link:"https://www.argusleader.com/gcdn/presto/2020/09/30/PDEM/2ac4ee57-3faa-4e47-9348-c7756a7fba6e-cas_brn_tm_rgb_pos_1.jpg"
+  
+  
+    },
+  
+    {
+      city: "Maryville",
+      state:"MO",
+      review:4.5,
+      rest_name:"Hangers",
+      Items:[
+        "Hangar 1 Classic Burger",
+    "Crispy Chicken Sandwich",
+    "Hangar 1 Wings",
+    "BBQ Ribs",
+    "Hangar 1 Salad",
+    "Grilled Cheese",
+    "Fish Tacos",
+    "Hangar 1 Nachos",
+    "Buffalo Chicken Wrap",
+    "Hangar 1 Fries"
+      ],
+      prep_time: "5min",
+      img_link:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMIiv_2NBu20sYqMV1gilbn9ETCdQhDg3JAr70-QJFGRdK02_cj0hN7Ju2Y-rXxzyiWaI&usqp=CAU"
+  
+  
+    },
+    
+    {
+      city: "Maryville",
+      state:"MO",
+      review:4.5,
+      rest_name:"Pizza Ranch",
+      Items:[
+        "Buffet Pizza",
+        "Chicken Wings",
+        "Chicken Tenders",
+        "Ranch Stix",
+        "Cactus Bread",
+        "Buffalo Chicken Pizza",
+        "Hawaiian Pizza",
+        "Taco Pizza",
+        "Pepperoni Pizza",
+        "Cheeseburger Pizza"
+      ],
+      prep_time: "5min",
+      img_link:"https://visitnorfolkne.com/wp-content/uploads/2020/02/pizza-ranch-logo.png"
+  
+  
+    },
+  
+    {
+      city: "Maryville",
+      state:"MO",
+      review:4.5,
+      rest_name:"A & G",
+      Items:[
+        "A&G Special Pizza",
+    "Pepperoni Pizza",
+    "Margherita Pizza",
+    "Veggie Pizza",
+    "BBQ Chicken Pizza",
+    "Cheeseburger Pizza",
+    "Garlic Knots",
+    "Chicken Caesar Salad",
+    "Buffalo Wings",
+    "Mozzarella Sticks"
+      ],
+      prep_time: "5min",
+      img_link:"https://assets.earthdiver.com/media/3115432.jpg?xxx"
+  
+  
+    }
+  
+  
+  
+  
+  
+  ];
+
+
+  export default rest_data;
