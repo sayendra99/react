ccb3e8d86a4db42030e3bda8b65b0ea92b0de4ea
Devabhaktineni Sayendranadh Chowdary <devabhaktinenisayendra@gmail.com>
2024-09-04 14:21:13 -0500

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,17 @@
+import React, { useState } from "react";
+import Restcomp from "./Restcomp";
+import rest_data from "../utils/mock";
 const Body=()=>{
 
     return(
       <div className="Body">
-        <div className="Search">Search</div>
+             <div className="Filter">
+        <button className="filter-button">
+          Top Restaurants
+        </button>
+      </div>
+
+
         <div className="rest-container">
         {rest_data.map((restaurant, index) => (
               <Restcomp
