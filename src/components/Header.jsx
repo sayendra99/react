3fc22c289fd92232fe34758a929bf56f9f241e00
Devabhaktineni Sayendranadh Chowdary <devabhaktinenisayendra@gmail.com>
2024-09-11 10:06:41 -0500

    useSet Is Applied To Log Button Of The header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,34 @@
-
 import { logoUrl } from "../utils/contants";
-const Header =() =>{
-    return (
-  <div className="header">  {/* Updated className to match the CSS */}
-        <div>
-          <img
-            className="logo"
-            src={logoUrl}
-            alt="Pizza Logo"
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+import { useState } from "react";
+
+const Header = () => {
+  // State to manage the button text (login/logout)
+  const [Btn_Log, SetBtn_Log] = useState("login");
+
+  return (
+    <div className="header">
+      <div>
+        <img className="logo" src={logoUrl} alt="Pizza Logo" />
       </div>
-  )
-  }
-export default Header;  
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="log-btn"
+            onClick={() => {
+              // Toggle between login and logout
+              Btn_Log === "login" ? SetBtn_Log("logout") : SetBtn_Log("login");
+            }}
+          >
+            {Btn_Log}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
