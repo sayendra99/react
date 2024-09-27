81d740c6d14720eebb8f5d8e93e81b4ea6a42ad1
Devabhaktineni Sayendranadh Chowdary <devabhaktinenisayendra@gmail.com>
2024-09-27 13:15:10 -0500

    Created Class Component which will be used at About page

---
+++ src/components/UserClass.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+class UserClass extends  React.Component
+{
+    constructor(props)
+    {
+
+    }
+
+
+
+
+
+
+
+
+    render(){
+        return
+        (
+
+
+            <div className="User-class">
+                <h1>Count : {}</h1>
+                <h2>Name:{}</h2>
+                <h3>Email:{}</h3>
+                <h4>Location:{}</h4>
+
+
+            </div>
+
+        );
+    }
+
+}
