b8b7298a1cef0e4582a7a9f46145d806e9493917
Devabhaktineni Sayendranadh Chowdary <devabhaktinenisayendra@gmail.com>
2024-09-27 13:37:36 -0500

    Successfully class component is called with Props

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,19 @@
+import UserClass from "./UserClass";
+
+
 const About=()=>
     {
         return(
         <div>
-        
+        <UserClass
+        //props passing
+        Count={5} 
+        Name="John Doe" 
+        Email="john.doe@example.com" 
+        Location="New York"
+        />
             
-            </div>)
+        </div>)
 
     }
 
