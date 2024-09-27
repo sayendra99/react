152a2fcd75e94c68941ea3c37759e5560d8c03a4
Devabhaktineni Sayendranadh Chowdary <devabhaktinenisayendra@gmail.com>
2024-09-27 13:28:07 -0500

    Syntax issue updated

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -1,35 +1,24 @@
 import React from "react";
 
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-class UserClass extends  React.Component
-{
-    constructor(props)
-    {
+  render() {
+    // Destructure the props object to extract the required properties
+    const { Count, Name, Email, Location } = this.props;
 
-    }
-
-
-
-
-
-
-
-
-    render(){
-        return
-        (
-
-
-            <div className="User-class">
-                <h1>Count : {}</h1>
-                <h2>Name:{}</h2>
-                <h3>Email:{}</h3>
-                <h4>Location:{}</h4>
-
-
-            </div>
-
-        );
-    }
-
+    // The return statement should be followed directly by the JSX block
+    return (
+      <div className="User-class">
+        <h1>Count: {Count}</h1>
+        <h2>Name: {Name}</h2> {/* Corrected the typo from Namw to Name */}
+        <h3>Email: {Email}</h3>
+        <h4>Location: {Location}</h4>
+      </div>
+    );
+  }
 }
+
+export default UserClass;
